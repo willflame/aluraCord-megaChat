0051c2cbae984842b5edd9728519474b760114b7
Willian Anselmo da Silva <willian0506@hotmail.com>
2022-01-30 17:22:32 -0300

    Aula-05, funcinalidade de exclusãode menssagem

--- pages/chat.js
+++ pages/chat.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { Box, Text, TextField, Image, Button } from "@skynexui/components";
+import { Box, TextField, Button } from "@skynexui/components";
 
 import { useRouter } from "next/router";
 import { createClient } from "@supabase/supabase-js";
 
 import appConfig from "../config.json";
+import { Header } from "../src/components/header";
+import { MessageList } from "../src/components/messageList";
+import { ButtonSendSticker } from "../src/components/buttonSendSticker";
 
 const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzQ4MTI1OSwiZXhwIjoxOTU5MDU3MjU5fQ.OstSKsK9Baxcd62fqE2vd-obaNAFIH2rt33ZsgyyTtg";
@@ -12,20 +15,50 @@ const SUPABASE_URL = "https://msptzzofgtwenlyaslme.supabase.co";
 
 const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
+function listnerPosts(addNewPost) {
+  return supabaseClient
+    .from("posts")
+    .on("*", (data) => {
+      const post = {
+        id: data.eventType === "DELETE" ? data.old.id : data.new.id,
+        ...data.new,
+      };
+      
+      addNewPost({ type: data.eventType, post });
+    })
+    .subscribe();
+}
+
 export default function ChatPage() {
   const router = useRouter();
-  const authenticatedUser = router.query.username || '';
+  const authenticatedUser = router.query.username || "";
   const [message, setMessage] = React.useState("");
   const [listPosts, setListPosts] = React.useState([]);
 
   React.useEffect(async () => {
-    await supabaseClient
+    supabaseClient
       .from("posts")
       .select("*")
       .order("id", { ascending: false })
       .then(({ data }) => {
         setListPosts(data);
       });
+
+    const subscription = listnerPosts((data) => {
+      const { type, post } = data;
+      setListPosts((listPosts) => {
+        if (type === "DELETE") {
+          const newList = listPosts.filter((item) => item.id !== post.id);
+          return newList;
+        }
+
+        return [post, ...listPosts];
+      });
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
   }, []);
 
   function handlerMessage(event) {
@@ -33,7 +66,7 @@ export default function ChatPage() {
     setMessage(newMessage);
   }
 
-  function sendMessage() {
+  function sendMessage(message) {
     if (message.length > 0) {
       const newPost = {
         from: authenticatedUser,
@@ -44,14 +77,21 @@ export default function ChatPage() {
     }
   }
 
+  function sendSticker(sticker) {
+    sendMessage(`:sticker: ${sticker}`);
+  }
+
   async function insertNewPost(post) {
     await supabaseClient
       .from("posts")
       .insert([post])
-      .then(({ data }) => {
-        setListPosts([data[0], ...listPosts]);
+      .then(() => {
         setMessage("");
       });
+  }
+
+  async function deletePost(id) {
+    await supabaseClient.from("posts").delete().eq("id", id);
   }
 
   return (
@@ -95,7 +135,11 @@ export default function ChatPage() {
             padding: "16px",
           }}
         >
-          <MessageList listPosts={listPosts} />
+          <MessageList
+            listPosts={listPosts}
+            authenticatedUser={authenticatedUser}
+            onDeletePost={deletePost}
+          />
           <Box
             as="form"
             styleSheet={{
@@ -110,7 +154,7 @@ export default function ChatPage() {
               onKeyPress={(event) => {
                 if (event.key === "Enter") {
                   event.preventDefault();
-                  sendMessage();
+                  sendMessage(message);
                 }
               }}
               placeholder="Insira sua mensagem aqui..."
@@ -126,6 +170,11 @@ export default function ChatPage() {
                 color: appConfig.theme.colors.neutrals[200],
               }}
             />
+
+            <ButtonSendSticker
+              onStickerClick={sendSticker}
+            />
+
             <Button
               iconName="FaTelegramPlane"
               label=""
@@ -135,100 +184,15 @@ export default function ChatPage() {
                 event.preventDefault();
                 sendMessage();
               }}
+              styleSheet={{
+                marginLeft: "6px",
+                width: "50px",
+                marginBottom: "8px",
+              }}
             />
           </Box>
         </Box>
       </Box>
-    </Box>
-  );
-}
-
-function Header() {
-  return (
-    <>
-      <Box
-        styleSheet={{
-          width: "100%",
-          marginBottom: "16px",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-        }}
-      >
-        <Text variant="heading5">Chat</Text>
-        <Button
-          variant="tertiary"
-          colorVariant="neutral"
-          label="Logout"
-          href="/"
-        />
-      </Box>
-    </>
-  );
-}
-
-function MessageList(props) {
-  function deletePost(id) {
-      supabaseClient.from('posts').delete().eq('id', id);
-  }
-
-  return (
-    <Box
-      tag="ul"
-      styleSheet={{
-        overflow: "scroll",
-        display: "flex",
-        flexDirection: "column-reverse",
-        flex: 1,
-        color: appConfig.theme.colors.neutrals["000"],
-        marginBottom: "16px",
-      }}
-    >
-      {props.listPosts.map((post) => {
-        return (
-          <Text
-            key={post.id}
-            tag="li"
-            styleSheet={{
-              borderRadius: "5px",
-              padding: "6px",
-              marginBottom: "12px",
-              hover: {
-                backgroundColor: appConfig.theme.colors.neutrals[700],
-              },
-            }}
-          >
-            <Box
-              styleSheet={{
-                marginBottom: "8px",
-              }}
-            >
-              <Image
-                styleSheet={{
-                  width: "20px",
-                  height: "20px",
-                  borderRadius: "50%",
-                  display: "inline-block",
-                  marginRight: "8px",
-                }}
-                src={`https://github.com/${post.from}.png`}
-              />
-              <Text tag="strong">{post.from}</Text>
-              <Text
-                styleSheet={{
-                  fontSize: "10px",
-                  marginLeft: "8px",
-                  color: appConfig.theme.colors.neutrals[300],
-                }}
-                tag="span"
-              >
-                {post.created_at}
-              </Text>
-            </Box>
-            {post.message}
-          </Text>
-        );
-      })}
     </Box>
   );
 }
