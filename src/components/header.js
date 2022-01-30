dbd784ba8671f3d667ac9aae9ba20a7a63a101fd
Willian Anselmo da Silva <willian0506@hotmail.com>
2022-01-30 17:20:31 -0300

    Separaçãpo dos componentes existentes

---
+++ src/components/header.js
@@ -0,0 +1,25 @@
+import { Box, Button, Text } from "@skynexui/components";
+
+export function Header() {
+    return (
+      <>
+        <Box
+          styleSheet={{
+            width: "100%",
+            marginBottom: "16px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text variant="heading5">Chat</Text>
+          <Button
+            variant="tertiary"
+            colorVariant="neutral"
+            label="Logout"
+            href="/"
+          />
+        </Box>
+      </>
+    );
+  }
---
+++ src/components/title.js
@@ -0,0 +1,17 @@
+import appConfig from "../../config.json";
+
+export function Title(props) {
+    const Tag = props.tag || "h1";
+    return (
+      <>
+        <Tag>{props.children}</Tag>
+        <style jsx>{`
+          ${Tag} {
+            color: ${appConfig.theme.colors.neutrals["000"]};
+            font-size: 24px;
+            font-weight: 600;
+          }
+        `}</style>
+      </>
+    );
+  }
